495f87ba2814c0be7fa3c4c09d6a049ffe8d5292
Dave-d-1st <Daveman8282@gmail.com>
2024-08-28 19:10:44 +0100

    Fourth Commit

--- render.py
+++ render.py
@@ -2,6 +2,8 @@ import os
 import socket
 from multiprocessing import Process
 
+html="""
+"""
 # Function to start the socket server
 def start_socket_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,11 +19,11 @@ def start_socket_server():
         print(f"Connection from {addr}")
         client_socket.sendall(b"Hello from socket server!")
         client_socket.close()
-
 # The WSGI callable application
 def app(environ, start_response):
+    print(environ)
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"Socket server is running."]
+    return [b"Socket server is ruing."]
 
 # Start the socket server in a separate process when the module is run
 if __name__ == "_main_":
