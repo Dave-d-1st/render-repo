47e6e27ad9faf250a0ea733aea654635db836dd7
Dave-d-1st <Daveman8282@gmail.com>
2024-08-29 17:21:16 +0100

    Switching to flask

--- render.py
+++ render.py
@@ -1,37 +1,19 @@
-import os
-import socket
-from multiprocessing import Process
+from flask import Flask, request, jsonify,redirect
 
-html="""
-"""
-# Function to start the socket server
-def start_socket_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '0.0.0.0'
-    port = int(os.environ.get('PORT', 8080))
-    server.bind((host, port))
-    server.listen(5)
+app=Flask(__name__)
 
-    print(f"Socket server listening on {host}:{port}")
-    
-    while True:
-        client_socket, addr = server.accept()
-        print(f"Connection from {addr}")
-        client_socket.sendall(b"Hello from socket server!")
-        client_socket.close()
-# The WSGI callable application
-def app(environ, start_response):
-    print(environ)
-    print("Deez")
-    print(start_response)
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"Socket server is ruing."]
 
-# Start the socket server in a separate process when the module is run
-if __name__ == "_main_":
-    # Start the socket server in a new process
-    p = Process(target=start_socket_server)
-    p.start()
-    
-    # WSGI app callable for Gunicorn
-    # Gunicorn will invoke the app function to serve HTTP requests
+@app.route('/get/<user>')
+def home(user):
+    user_data={
+        'id':user,
+        'b':'alls'
+    }
+    extra=request.args.get("extra")
+    if extra:
+        user_data['extra'] = extra
+    print(extra)
+    return jsonify(user_data), 200
+
+if __name__=="__main__":
+    app.run(debug=True)
