22caa8832a35ecef8763b6081b383d937c31040b
Dave-d-1st <Daveman8282@gmail.com>
2025-06-25 17:30:53 +0100

    De

--- render.py
+++ render.py
@@ -1,11 +1,16 @@
-from flask import Flask,request,session,jsonify
+from flask import Flask,send_file,request
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods = ["POST"])
 def reg():
-    return "Posted", 201
-
+    with open("s.txt",'w') as f:
+        f.write("Butter")
+    return "Balls", 200
+@app.route('/')
+def ge():
+    with open("s.txt") as f:
+        return f.read(),200
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True,port=35586)
